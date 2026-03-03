31aba3ded03e87f8e0b4cf505db571867f81198c
Parth Pandya <154543191+parthpandya-qt@users.noreply.github.com>
2026-03-03 19:12:23 +0000

    datatypes and type conversion

---
+++ basics/2constandvar.js
@@ -0,0 +1,15 @@
+const Name = "Parth Pandya"
+
+var city = "Ujjain"
+
+let Pincode = 456443
+
+Hobby  = "Cricket"
+
+console.table([Name , city , Pincode , Hobby])
+
+//const remail constant
+
+// their is two way to declare variable var and let
+
+// Always prefer to use let not var 
---
+++ basics/3dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all the JS code as newer version
+
+//alert(3+3) // we are using nodejs not browser so error will occure here not on the browser
+
+
+console.log(3
+    +3) // code readability should be high
+
+let name = "parth pandya"  //string
+
+let age=20 //in javascript it is called Number its range is 2^53
+
+let isLoggedIn=false // boolean
+
+// bigint it is type of data type which stores very very high number
+// null => standalone value
+// undefined => value is not defined
+
+//symbole => used in uniqueness of value
+
+
+//above all is premetive
+
+
+//object, array , Dates etc
+
+console.log(typeof(null))   // output will be object
+console.log(typeof(undefined)) // output will be undefined
+
